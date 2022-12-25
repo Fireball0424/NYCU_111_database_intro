63db50aceeefb58ab4b093888e4396dfa061027c
Fireball0424 <65496023+Fireball0424@users.noreply.github.com>
2022-12-26 02:02:05 +0800

    Add files via upload

---
+++ find_all_skills.R
@@ -0,0 +1,12 @@
+## import the library
+library(dplyr)
+
+## read the data
+df <- read.csv("/home/fireball/Desktop/database_intro/final project/skills_database.csv")
+
+## distinct 
+ndf <- distinct(df, Skills)
+
+View(ndf)
+
+write.csv(ndf, "/home/fireball/Desktop/database_intro/final project/all_skills.csv", row.names = FALSE)
