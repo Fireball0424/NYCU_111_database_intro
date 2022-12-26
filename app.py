5e4bf7911c89d1410cb801696c8637f7c5f82093
Yi Chung Tseng <kuroregan@yahoo.com>
2022-12-26 17:09:41 +0800

    Shows query result in def foo

--- app.py
+++ app.py
@@ -5,8 +5,11 @@ from flask import redirect
 from flask import request
 from flask import jsonify
 import json
+from database import Database
 
 app = Flask(__name__)
+database = Database()
+
 
 @app.route('/')
 def index():
@@ -15,6 +18,8 @@ def index():
 @app.route('/route_function', methods=['POST'])
 def foo():
     json = request.json
+    result = database.GetJobWithMultipleSkills(json)
+    print('Query Result: ', result)
     print('recv:', json)
     return json
 
